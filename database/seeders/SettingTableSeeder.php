dc97dcd911193c5df1ef91d5b6c246cf3bcb5da8
M.KHOLIKUL ANAM ADE .K_E41202189 <50694980+anamade@users.noreply.github.com>
2023-09-21 11:39:02 +0700

    commit
    kerangka aplikasi

---
+++ database/seeders/SettingTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('setting')->insert([
+            'id_setting' => 1,
+            'nama_perusahaan' => 'narasumber.id',
+            'alamat' => 'PLUT KUMKM Kota Batu - Jalan Abdul Gani Atas - Ngaglik - Kec. Batu ',
+            'telepon' => ' 0857-4966-5700',
+            'tipe_nota' => 1, // kecil
+            'diskon' => 5,
+            'path_logo' => '/img/logo.png',
+            'path_kartu_member' => '/img/member.png',
+        ]);
+    }
+}
